1368f9f114b2b24f0bc6ab9fd2b299076744ed13
N. M. Luna <nat.luna.64@gmail.com>
2022-11-24 09:27:19 -0800

    add problem 1 of Car Prototypes

--- carProto.js
+++ carProto.js
@@ -6,6 +6,32 @@
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
 
+//Car constructor
+class Car {
+    constructor(model) {
+      this.model = model;
+      this.currentSpeed = 0;
+    }
+
+    accelerate() {
+        this.currentSpeed++;
+    }
+
+    brake() {
+      this.currentSpeed--;
+    }
+  
+    toString() { 
+      return `It's a ${this.model}, driving at ${this.currentSpeed} units of speed.`
+    }
+}
+
+const myFirstCar = new Car('Tercel')
+myFirstCar.accelerate()
+myFirstCar.accelerate()
+myFirstCar.break()
+console.log(myFirstCar.toString())
+
 /**
  * ElectricCar class
  * @constructor
