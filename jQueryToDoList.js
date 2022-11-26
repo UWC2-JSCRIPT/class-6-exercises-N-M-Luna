cf9a4685ca3d508a278d3dd92ed36cb83d8cf39d
N. M. Luna <nat.luna.64@gmail.com>
2022-11-26 15:57:22 -0800

    add problem 3 of to do list

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -23,11 +23,27 @@ $('.delete').click(function() {
 /**
  * Adds new list item to <ul>
  */
+
 const addListItem = function(e) {
   e.preventDefault();
+  // Create the description element,
   const text = $('input').val();
+  const $description = $('<span>')
+    .text(text)
 
-  // rest here...
+  // and the delete element.
+  const $deleteBtn = $('<a>')
+    .text('Delete') 
+    .addClass('delete')
+
+  // Build the new element.
+  const $newItem = $('<li>')
+    .append($description)
+    .append($deleteBtn)
+
+  // Place it at the end of the UL tag
+  $( 'ul' ).append( $newItem )
 };
 
-// add listener for add
+// Add listener for .add-item A tag
+$('.add-item').click(addListItem)
